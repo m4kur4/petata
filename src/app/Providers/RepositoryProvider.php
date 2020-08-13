a6fed396ad6f39ede6ecb6ce9235c702a5bb3fee
016 <i.shiiba.016i@gmail.com>
2020-08-13 17:08:31 +0900

    [MOD]リポジトリのサービスプロバイダーへバインダーリポジトリを追加設定

--- src/app/Providers/RepositoryProvider.php
+++ src/app/Providers/RepositoryProvider.php
@@ -13,9 +13,15 @@ class RepositoryProvider extends ServiceProvider
      */
     public function register()
     {
+        // ユーザーリポジトリ
         $this->app->bind(
             \App\Repositories\Interfaces\UserRepositoryInterface::class,
             \App\Repositories\UserRepository::class
+        );
+        // バインダーリポジトリ
+        $this->app->bind(
+            \App\Repositories\Interfaces\BinderRepositoryInterface::class,
+            \App\Repositories\BinderRepository::class
         );
     }
 
