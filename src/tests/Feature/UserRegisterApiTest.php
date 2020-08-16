22bf7067e7cf3eb201f1d01d61eccf358bae3904
makura016 <i.shiiba.016i@gmail.com>
2020-08-16 17:04:17 +0900

    [MOD]認証に自作のUserモデルを使用する変更に併せてテストコード修正
    ・ログインAPIテスト
    ・ユーザー登録APIテスト

--- src/tests/Feature/UserRegisterApiTest.php
+++ src/tests/Feature/UserRegisterApiTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Feature;
 
-use App\User;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+
 use Tests\TestCase;
 
 use Hash;
