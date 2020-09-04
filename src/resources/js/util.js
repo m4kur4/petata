1283a3b8416acdfa5febfd2ee3f70062a8e0f727
makura016 <i.shiiba.016i@gmail.com>
2020-09-05 07:51:47 +0900

    [MOD]バインダー画像のクリップボードコピー機能を改修
    ・jsオブジェクトにBlobをキャッシュすることで、ボタンを押下するたびにリクエストが飛ぶことがないように変更

--- src/resources/js/util.js
+++ src/resources/js/util.js
@@ -1,10 +1,28 @@
 export const util = {
     /**
+     * キャッシュ
+     */
+    cache: {
+        image: {}
+    },
+    /**
      * 指定した画像をクリップボードにコピーします。
      */
-    async copyImageToClipBoard(image) {
-        const img = await fetch(image.src);
-        const imgBlob = await img.blob();
+    async copyImageToClipBoard(image, imageId) {
+        const hasCache = !!util.cache.image[imageId];
+
+        let imgBlob = null;
+        if (hasCache) {
+            // キャッシュがある場合
+            imgBlob = util.cache.image[imageId];
+        } else {
+            // キャッシュがない場合は格納
+            const img = await fetch(image.src);
+            imgBlob = await img.blob();
+            util.cache.image[imageId] = imgBlob;
+            console.log("hoge");
+        }
+
         try {
            navigator.clipboard.write([
              new ClipboardItem({
