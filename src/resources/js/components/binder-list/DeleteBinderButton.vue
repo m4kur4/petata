4ad102fadef934c5ee4b475f7029384978becaf7
makura016 <i.shiiba.016i@gmail.com>
2020-09-10 11:10:11 +0900

    [MOD]バインダー一覧画面のボタンへスタイルを適用

--- src/resources/js/components/binder-list/DeleteBinderButton.vue
+++ src/resources/js/components/binder-list/DeleteBinderButton.vue
@@ -3,7 +3,6 @@
         <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
-            fill="#324750"
             width="64px"
             height="64px"
         >
